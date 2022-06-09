036fc7f166fbfa2e71c452cef832dde985feaf89
Vanesa <v.zimara@gmail.com>
2022-06-09 21:09:19 +0200

    change background img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,11 @@ import { RestaurantCard } from "../components/RestaurantCard";
 
 const styles = {
   paperContainer: {
-    width: "100%",
-    height: "100vh",
-    objectFit: "contain",
-    objectPosition: "right",
+    //width: "100%",
+    height: "98vh",
+    backgroundSize: "cover",
+    overflow: "hidden",
+    backgroundPosition: "center",
     backgroundImage: `url(https://images.unsplash.com/photo-1486485764572-92b96f21882a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80)`,
   },
 };
@@ -16,10 +17,10 @@ const styles = {
 export const Home = () => {
   return (
     <>
-      <div style={styles.paperContainer}>
+      <body style={styles.paperContainer}>
         <HeaderImg />
         <RestaurantList />
-      </div>
+      </body>
     </>
   );
 };
