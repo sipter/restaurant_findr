4e723496e4d8c74671ca8b6521ea247486b20e1c
Vanesa <v.zimara@gmail.com>
2022-06-09 20:35:39 +0200

    add background img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,23 @@ import { HeaderImg } from "../components/HeaderImg";
 import { RestaurantList } from "../components/RestaurantList";
 import { RestaurantCard } from "../components/RestaurantCard";
 
+const styles = {
+  paperContainer: {
+      height: 1356,
+      backgroundImage: `url(${"static/src/img/main.jpg"})`
+  }
+};
+
 export const Home = () => {
   return (
-    <>
-      <HeaderImg />
+    < >
+    <div style={styles.paperContainer} >
+       <HeaderImg />
       <RestaurantList />
       <RestaurantCard />
+    </div>
+     
     </>
   );
 };
+
